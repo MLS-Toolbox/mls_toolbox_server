78b3095db3cc9037c925eefaae5159b37005ae18
MikoMikarro <lopezcuestam@gmail.com>
2024-08-15 10:31:17 +0200

    added endpoints for code assessment

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
 """ server.py : Flask server for mls_toolbox. """
 
 import requests
+import os
 
 from flask import Flask, request
 from flask_cors import cross_origin, CORS
 from waitress import serve
-import os
 
 app = Flask(__name__)
 
 @app.route('/api/create_app', methods=['GET', 'POST'])
 @cross_origin()
-def request_app():
+def create_app():
     """
     Handles incoming HTTP requests to the '/api/create_app' endpoint.
 
@@ -44,7 +44,7 @@ def request_app():
 
 @app.route('/api/test_create_app', methods=['GET', 'POST'])
 @cross_origin()
-def test_request_app():
+def test_create_app():
     """
     Handles incoming HTTP requests to the '/api/test_create_app' endpoint.
 
@@ -64,7 +64,7 @@ def test_request_app():
         os.getenv("MLS_CODE_GENERATOR_PORT", "5050")
 
     reponse = requests.request(
-        method = "POST",
+        method = "GET",
         url = target_url,
         headers =
             {
@@ -73,6 +73,72 @@ def test_request_app():
         timeout = 10
         )
     return reponse.text
+
+@app.route('/api/rate_app', methods=['GET', 'POST'])
+@cross_origin()
+def rate_app():
+    """
+    Handles incoming HTTP requests to the '/api/rate_app' endpoint.
+
+    Supports both GET and POST methods.
+
+    Makes a POST request to the 'rate_app' endpoint
+    with the provided JSON data and returns the response content.
+
+    Parameters:
+        None
+
+    Returns:
+        The content of the response from the target URL.
+    """
+    target_url = "http://" + os.getenv("MLS_CODE_ASSESS_URI", "localhost") + ":" + \
+        os.getenv("MLS_CODE_ASSESS_PORT", "5060") + "/api/test_rate_app"
+    response = requests.request(
+        method="POST",
+        url = target_url,
+        json = request.json,
+        headers =
+            {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+        timeout=100
+    )
+    return response.content
+
+@app.route('/api/test_rate_app', methods=['GET', 'POST'])
+@cross_origin
+def test_rate_app():
+    """
+    Handles incoming HTTP requests to the '/api/test_rate_app' endpoint.
+
+    Supports both GET and POST methods.
+
+    Makes a GET request to the 'test_rate_app' endpoint
+    and returns the response content.
+
+    Parameters:
+        None
+
+    Returns:
+        The content of the response from the target URL.
+    """
+
+    target_url = "http://" + os.getenv("MLS_CODE_ASSESS_URI", "localhost") + ":" + \
+        os.getenv("MLS_CODE_ASSESS_PORT", "5060") + "/api/test_rate_app"
+
+    response = requests.request(
+        method = "GET",
+        url = target_url,
+        headers =
+            {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+        timeout = 10
+        )
+
+    return response.text
 
 @app.route('/api/get_config', methods=['GET', 'POST'])
 @cross_origin()
